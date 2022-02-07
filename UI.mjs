7eecbcec82f09c646b9eb1d521b9074fea16c475
Cristian Talau <cristitalau@gmail.com>
2022-02-07 10:31:50 -0800

    #3 Populate the next suggestion

---
+++ UI.mjs
@@ -0,0 +1,36 @@
+export default class UI {
+    getGuess(index) {
+        return document.getElementById("guess-" + index).value;
+    }
+    setNextGuess(value) {
+        for (var i = 1; i <= 6; i++) {
+            if (this.getGuess(i) === '') {
+                document.getElementById("guess-" + i).value = value;
+                break;
+            }
+        }
+    }
+    getInfo(index) {
+        return document.getElementById("info-" + index).value;
+    }
+
+    getConstraints() {
+        var constraints = [];
+        for (var i = 1; i <= 6; i++) {
+            var guess = this.getGuess(i);
+            if (guess.length > 0) {
+                var info = this.getInfo(i);
+                if (/[_gy]{5}/.test(info)) {
+                    constraints.push({guess, info});
+                } else {
+                    alert('Invalid info: ' + info);
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        return constraints;
+    }
+    
+}
--- index.mjs
+++ index.mjs
@@ -1,32 +1,19 @@
 import ConstraintSet from "./ConstraintSet.mjs";
 import { WORDS } from "./wordlist.mjs";
 import Responder from "./Responder.mjs";
+import UI from "./UI.mjs";
 
+var ui = new UI();
 
-function createConstraintSet() {
-    var constraints = [];
-    for (var i = 1; i <= 6; i++) {
-        var guess = document.getElementById("guess-" + i).value;
-        var info = document.getElementById("info-" + i).value;
-        if (guess.length > 0) {
-            console.log(guess, info);
-            constraints.push({guess, info});
-        } else {
-            break;
-        }
-    }
+document.getElementById('submit').onclick = () => {    
+    var constraintSet =  ConstraintSet.fromGuesses(ui.getConstraints());    
     
-    return ConstraintSet.fromGuesses(constraints);
-}
-
-function extractFirstNElements(array, n) {
-    return array.slice(0, n);
-}
-
-document.getElementById('submit').onclick = () => {
-    var constraintSet = createConstraintSet();
     var candidates = WORDS.filter(word => constraintSet.matches(word));
-    var suggestion = new Responder(candidates).getGuessWithBestMostFrequestResponse();
-    alert(suggestion + '\n\n' + candidates.length + '\n' + candidates.slice(0, 5).map(word => ' - ' + word).join('\n'));
+    if (candidates.length === 0) {
+        alert('No matching words.');
+    } else {
+        var suggestion = new Responder(candidates).getGuessWithBestMostFrequestResponse();
+        ui.setNextGuess(suggestion);
+    }
 }
 
